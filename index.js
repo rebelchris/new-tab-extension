052e19058e601db9845a9d8eeec3826c3279287b
Chris Bongers <chrisbongers@gmail.com>
2022-08-05 07:55:21 +0200

    feat: introducing React

---
+++ index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import { App } from "./src/App";
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content:
-     ['*.html'],
+     ['src/*.js'],
   theme: {
     extend: {},
   },
