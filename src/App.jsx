13704f661b255f02e08090df71e078119c63636e
Chris Bongers <chrisbongers@gmail.com>
2022-08-08 08:14:31 +0200

    feat: added weather API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Counter from "./Counter";
+import Weather from "./Weather";
 
 export function App() {
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen bg-indigo-400 text-6xl font-bold text-white">
             <p>Welcome 👋</p>
+            <br />
+            <Weather />
             <br />
             <Counter />
         </div>
